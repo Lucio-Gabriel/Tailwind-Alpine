5464ce0f2fb8e37f706d8c371f141122a70f1634
Gabriel <suppershock777@gmail.com>
2024-11-11 17:51:28 -0300

    start aulas

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'roxo': '#8352EB',
+        'amarelo': '#DBC900',
+        'azul': '#011C40'
+      }
+    },
+  },
+  plugins: [],
+}
+
